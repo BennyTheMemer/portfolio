3a0673a685951637ffee1e07ca91f824a83032c1
BennyTheMemer <bernardolcc98@gmail.com>
2022-02-18 11:42:01 +0000

    Navbar

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import _navbar from "../sass/components/_navbar.scss";
+  import ChevronCircleUp from "svelte-icons/fa/FaBars.svelte";
+  import Drawer from "./Drawer.svelte";
+
+  let isDrawerOpen: boolean = false;
+
+  const toggleNav = function () {
+    isDrawerOpen = !isDrawerOpen;
+    console.log(isDrawerOpen);
+  };
+</script>
+
+<div>
+  <div>
+    <h1>Bernardo</h1>
+  </div>
+  <ul>
+    <li>
+      <a href="/About"> About</a>
+    </li>
+    <li><a href="/About"> Projects</a></li>
+    <li><a href="/About"> Contact</a></li>
+  </ul>
+  <Drawer isOpen={isDrawerOpen} />
+  <button on:click={toggleNav}> <ChevronCircleUp /> </button>
+</div>
