793efd3ce58c9135ca6098937b8e2d5016925971
BennyTheMemer <bernardolcc98@gmail.com>
2022-02-18 11:41:55 +0000

    Navbar

---
+++ src/components/Drawer.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let isOpen;
+</script>
+
+<main>
+  <div>
+    <ul>
+      <li>
+        <a href="/About"> About</a>
+      </li>
+      <li><a href="/About"> Projects</a></li>
+      <li><a href="/About"> Contact</a></li>
+    </ul>
+  </div>
+</main>
+
+<style>
+</style>
